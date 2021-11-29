ab93ee97d4aebf00873b97c4792695f428dbc551
Thales Morato <tha.s.morato@gmail.com>
2021-11-29 19:25:32 -0300

    refact: module imports

--- api-service/app.js
+++ api-service/app.js
@@ -1,31 +1,31 @@
-const createError = require('http-errors');
-const express = require('express');
-const logger = require('morgan');
+import createError from "http-errors";
+import express from "express";
+import logger from "morgan";
 
-const indexRouter = require('./routes/index');
+import indexRouter from "./routes/index";
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app;
+export default app;
