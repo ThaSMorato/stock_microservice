3cc0bbb7dcadfa9c3d510da7329b58df04c733fd
Thales Morato <tha.s.morato@gmail.com>
2021-11-28 21:11:57 -0300

    test: ApiStockRepository

---
+++ stock-service/repository/ApiStockRepository.test.js
@@ -0,0 +1,43 @@
+import { describe, it, jest, expect } from "@jest/globals";
+import { ApiStockRepository } from "./ApiStockRepository";
+
+describe("#Unit test ApiStockRepository", () => {
+  it("should call the api get method", async () => {
+    const mockedApiGet = jest.fn();
+
+    const api = {
+      get: mockedApiGet,
+    };
+
+    const id = "123";
+
+    const apiStockRepository = new ApiStockRepository({ api });
+
+    await apiStockRepository.getStockById(id);
+
+    expect(mockedApiGet).toHaveBeenCalledWith({ id });
+  });
+
+  it("should return a json with the id as a key", async () => {
+    const resolvedMockValue = [
+      {
+        key1: "Jhon",
+        key2: "Doe",
+      },
+    ];
+
+    const mockedApiGet = jest.fn().mockResolvedValue(resolvedMockValue);
+
+    const api = {
+      get: mockedApiGet,
+    };
+
+    const id = "123";
+
+    const apiStockRepository = new ApiStockRepository({ api });
+
+    const result = await apiStockRepository.getStockById(id);
+
+    expect(result).toStrictEqual({ [id]: resolvedMockValue });
+  });
+});
