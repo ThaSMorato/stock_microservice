e6b54d95b8a75a12fd901b0d2a2191dcad7ef7d2
Thales Morato <tha.s.morato@gmail.com>
2021-11-29 20:19:31 -0300

    test: create user controller

---
+++ api-service/modules/accounts/useCases/createUser/CreateUserController.test.js
@@ -0,0 +1,35 @@
+import { describe, jest, it, expect, beforeEach } from "@jest/globals";
+import { CreateUserController } from "./CreateUserController";
+
+describe("#CreateUserController", () => {
+  const createUserUseCase = {
+    execute: jest.fn(),
+  };
+
+  const user = {
+    password: "123",
+    login: "Jhon_Doe",
+    name: "John",
+    isAdmin: false,
+  };
+
+  const request = {
+    body: user,
+  };
+
+  const response = {
+    status: () => response,
+    send: jest.fn(),
+  };
+
+  beforeEach(() => jest.clearAllMocks());
+
+  it("should call execute and send on handler call", async () => {
+    const createUserController = new CreateUserController({ createUserUseCase });
+
+    await createUserController.handle(request, response);
+
+    expect(response.send).toBeCalled();
+    expect(createUserUseCase.execute).toBeCalled();
+  });
+});
