79ce7a8cd301265d9b416518340121da34c3d31a
Thales Morato <tha.s.morato@gmail.com>
2021-12-01 19:57:38 -0300

    refact: FindStockByIdController emit event

--- api-service/modules/stock/useCases/findStockById/FindStockByIdController.js
+++ api-service/modules/stock/useCases/findStockById/FindStockByIdController.js
@@ -1,4 +1,5 @@
 import { ApiError } from "../../../../Error/ApiError.js";
+import { StockResponseEventFactory } from "../../../../Events/index.js";
 
 export class FindStockByIdController {
   constructor({ findStockByIdUseCase }) {
@@ -7,12 +8,17 @@ export class FindStockByIdController {
 
   async handle(request, response) {
     const { q } = request.query;
+    const { user_id } = request;
 
     if (!q) throw new ApiError("Query must be provided", 400);
 
     const stock = await this.findStockByIdUseCase.execute({ id: q });
 
     if (!stock) throw new ApiError("Stock not found", 400);
+
+    const stockEventEmitter = StockResponseEventFactory.createInstance();
+
+    stockEventEmitter.emit({ stock, user_id });
 
     return response.status(200).json(stock);
   }
