a7fa8225d628d845b63e5a8aa531aec8de8dd29c
Thales Morato <tha.s.morato@gmail.com>
2021-12-01 20:23:10 -0300

    feat: history and stats routes

--- api-service/routes/user.routes.js
+++ api-service/routes/user.routes.js
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "../Middlewares/ensureAuthenticated.js";
+import { ensureIsAdmin } from "../Middlewares/ensureIsAdmin.js";
 import { AuthenticateUserFactory } from "../modules/accounts/useCases/authenticateUser/index.js";
 import { CreateUserFactory } from "../modules/accounts/useCases/createUser/index.js";
 import { NewPasswordFactory } from "../modules/accounts/useCases/newPassword/index.js";
+import { StockStatusFactory } from "../modules/stock/useCases/stockStatus/index.js";
+import { UserHistoryFactory } from "../modules/stock/useCases/userHistory/index.js";
 
 const userRouter = Router();
 
 const createUserHandler = await CreateUserFactory.createInstance();
 const authenticateUserHandler = await AuthenticateUserFactory.createInstance();
 const newPasswordHandler = await NewPasswordFactory.createInstance();
+const userHistoryHandler = await UserHistoryFactory.createInstance();
+const stockStatusHandler = await StockStatusFactory.createInstance();
 
 userRouter.post("/register", createUserHandler.handle.bind(createUserHandler));
 
@@ -18,6 +23,15 @@ userRouter.patch(
   "/users/reset/password",
   ensureAuthenticated,
   newPasswordHandler.handle.bind(newPasswordHandler)
+);
+
+userRouter.get("/history", ensureAuthenticated, userHistoryHandler.handle.bind(userHistoryHandler));
+
+userRouter.get(
+  "/stats",
+  ensureAuthenticated,
+  ensureIsAdmin,
+  stockStatusHandler.handle.bind(stockStatusHandler)
 );
 
 export { userRouter };
