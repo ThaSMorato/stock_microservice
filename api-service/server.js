db9ca3b7ed6f3f90ac67e431f5a033873e054943
Thales Morato <tha.s.morato@gmail.com>
2021-12-02 00:24:38 -0300

    feat: swagger route as /api-docs

--- api-service/server.js
+++ api-service/server.js
@@ -4,18 +4,24 @@ import "express-async-errors";
 import { NewPasswordEventFactory, StockResponseEventFactory } from "./Events/index.js";
 import { errorHandler } from "./Middlewares/errorMiddleware.js";
 
+import swaggerUI from "swagger-ui-express";
 import { router } from "./routes/index.js";
+import swaggerFile from "./swagger.json";
+
+const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
 app.use(router);
 
 app.use(errorHandler);
 
-app.listen(3001, () => {
-  console.log("listening on 3001");
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
   const newPasswordEvent = NewPasswordEventFactory.createInstance();
   const stockResponseEvent = StockResponseEventFactory.createInstance();
 
