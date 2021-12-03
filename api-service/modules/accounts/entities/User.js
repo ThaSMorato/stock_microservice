4b7bffdd390506a233d7d7ea44e40d8142eff48c
Thales Morato <tha.s.morato@gmail.com>
2021-12-03 15:44:59 -0300

    refact: entity rules

--- api-service/modules/accounts/entities/User.js
+++ api-service/modules/accounts/entities/User.js
@@ -1,8 +1,27 @@
+import { getRandomString } from "../../../utils/getRandomString.js";
+
 export class User {
-  constructor({ _id, email, password, isAdmin }) {
-    this.id = _id;
+  constructor({ _id, email, isAdmin }) {
     this.email = email;
-    this.password = password;
     this.isAdmin = isAdmin;
+    if (_id) {
+      this.id = _id;
+    } else {
+      this.password = getRandomString(8);
+    }
+  }
+
+  resetPassword() {
+    this.password = getRandomString(8);
+  }
+
+  get() {
+    const { id, email, isAdmin } = this;
+
+    return {
+      id,
+      email,
+      isAdmin,
+    };
   }
 }
